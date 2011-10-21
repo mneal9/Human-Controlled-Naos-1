659e6643a67c2b41242a1fc053934aeb6494d095
Caitlyn Clabaugh <ceclabaugh@gmail.com>
2011-10-21 14:58:41 -0400

    updated BodyFSM

--- Player/BodyFSM/NaoPlayer/bodySearch.lua
+++ Player/BodyFSM/NaoPlayer/bodySearch.lua
@@ -1,32 +1,48 @@
 module(..., package.seeall);
 
-require('gcm')
+require('Body')
+require('walk')
+require('vector')
 require('Config')
+require('wcm')
+
+t0 = 0;
+timeout = 10.0;
+
+-- turn velocity
+vSpin = 0.3;
+
+direction = 1;
 
 
 function entry()
-  print(_NAME.." forced entry");
+  print(_NAME.." entry");
+
+  t0 = Body.get_time();
+
+  -- set turn direction to last known ball position
+  ball = wcm.get_ball();
+  if (ball.y > 0) then
+    direction = 1;
+  else
+    direction = -1;
+  end
+end
 
 function update()
-  stateChange = gcm.get_fsm_body_state();
+  local t = Body.get_time();
 
-  if (stateChange = 'bodySearch') then
-    return "search2search";
+  ball = wcm.get_ball();
+
+  -- search/spin until the ball is found
+  walk.set_velocity(0, 0, direction*vSpin);
+
+  if (t - ball.t < 0.1) then
+    return "ball";
   end
-  if (stateChange = 'bodyApproach') then
-    return "approach2approach";
+  if (t - t0 > timeout) then
+    return "timeout";
   end
-  if (stateChange = 'bodyKick') then
-    return "kick2kick";
-  end
-  if (stateChange = 'bodyOrbit') then 
-    return "orbit2orbit";
-  end
-  if (stateChange = 'bodyCenter') then
-    return "center2center";
-  end
-  if (stateChange = 'bodyPosition') then
-    return "position2position
 end
 
 function exit()
